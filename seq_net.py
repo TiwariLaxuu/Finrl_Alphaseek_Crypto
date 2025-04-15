219267a77800d8060bfff72bf00f42f4de12d31d
tiwarilaxuu <tiwarilaxuu@gmail.com>
2025-04-15 22:56:12 +0545

    update code

--- seq_net.py
+++ seq_net.py
@@ -1,5 +1,6 @@
 import torch as th
 import torch.nn as nn
+import torch.nn.functional as F
 from typing import Tuple
 
 TEN = th.Tensor
@@ -88,7 +89,116 @@ class RnnRegNet(nn.Module):
     def get_obj_value(criterion, out: TEN, lab: TEN, wup_dim: int) -> TEN:
         obj = criterion(out, lab)[wup_dim:, :, :]
         return obj
+    
 
+
+
+class DilatedCNN(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=2):
+        super().__init__()
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, dilation=dilation, padding='same')
+        self.bn = nn.BatchNorm1d(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        return F.relu(x)
+
+class DilatedCNNRegNet(nn.Module):
+    def __init__(self, inp_dim, mid_dim, out_dim, num_layers=2, kernel_size=3, dilation=2):
+        super().__init__()
+        self.mlp_inp = NnSeqBnMLP((inp_dim, mid_dim, mid_dim), activation=nn.GELU())
+
+        # Two parallel CNN branches like LSTM and GRU in RNN version
+        self.cnn_branch1 = nn.ModuleList([
+            DilatedCNN(mid_dim, mid_dim, kernel_size=kernel_size, dilation=dilation)
+            for _ in range(num_layers)
+        ])
+        self.mlp1 = NnSeqBnMLP((mid_dim, mid_dim, mid_dim), activation=nn.GELU())
+
+        self.cnn_branch2 = nn.ModuleList([
+            DilatedCNN(mid_dim, mid_dim, kernel_size=kernel_size, dilation=dilation)
+            for _ in range(num_layers)
+        ])
+        self.mlp2 = NnSeqBnMLP((mid_dim, mid_dim, mid_dim), activation=nn.GELU())
+
+        self.mlp_out = NnSeqBnMLP((mid_dim * 2, mid_dim * 2, out_dim), activation=nn.Tanh())
+
+    def forward(self, inp, hid=None):
+        """
+        inp: shape [seq_len, batch_size, inp_dim]
+        """
+        # Match LSTM/GRU input shape convention
+        x = self.mlp_inp(inp)  # [seq_len, batch, mid_dim]
+        x = x.permute(1, 2, 0)  # [batch, mid_dim, seq_len] for Conv1d
+
+        out1 = x
+        for layer in self.cnn_branch1:
+            out1 = layer(out1)
+        out1 = out1.permute(2, 0, 1)  # [seq_len, batch, mid_dim]
+        tmp1 = self.mlp1(out1)
+
+        out2 = x
+        for layer in self.cnn_branch2:
+            out2 = layer(out2)
+        out2 = out2.permute(2, 0, 1)
+        tmp2 = self.mlp2(out2)
+
+        tmp = th.cat((tmp1, tmp2), dim=2)
+        out = self.mlp_out(tmp)
+        return out, None  # No hidden state for CNNs
+    
+    @staticmethod
+    def get_obj_value(criterion, out: TEN, lab: TEN, wup_dim: int) -> TEN:
+        obj = criterion(out, lab)[wup_dim:, :, :]
+        return obj
+    
+class TransformerEncoderBlock(nn.Module):
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1):
+        super().__init__()
+        self.encoder = nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout, batch_first=False)
+
+    def forward(self, x):
+        return self.encoder(x)
+
+class TransformerRegNet(nn.Module):
+    def __init__(self, inp_dim, mid_dim, out_dim, num_layers=2, nhead=4):
+        super().__init__()
+        self.mlp_inp = NnSeqBnMLP((inp_dim, mid_dim, mid_dim), activation=nn.GELU())
+
+        # Two parallel transformer stacks
+        self.transformer1 = nn.Sequential(*[
+            TransformerEncoderBlock(mid_dim, nhead) for _ in range(num_layers)
+        ])
+        self.mlp1 = NnSeqBnMLP((mid_dim, mid_dim, mid_dim), activation=nn.GELU())
+
+        self.transformer2 = nn.Sequential(*[
+            TransformerEncoderBlock(mid_dim, nhead) for _ in range(num_layers)
+        ])
+        self.mlp2 = NnSeqBnMLP((mid_dim, mid_dim, mid_dim), activation=nn.GELU())
+
+        self.mlp_out = NnSeqBnMLP((mid_dim * 2, mid_dim * 2, out_dim), activation=nn.Tanh())
+
+    def forward(self, inp, hid=None):
+        """
+        inp: shape [seq_len, batch_size, inp_dim]
+        """
+        x = self.mlp_inp(inp)
+
+        tmp1 = self.transformer1(x)
+        tmp1 = self.mlp1(tmp1)
+
+        tmp2 = self.transformer2(x)
+        tmp2 = self.mlp2(tmp2)
+
+        tmp = th.cat((tmp1, tmp2), dim=2)
+        out = self.mlp_out(tmp)
+        return out, None
+    
+    @staticmethod
+    def get_obj_value(criterion, out: TEN, lab: TEN, wup_dim: int) -> TEN:
+        obj = criterion(out, lab)[wup_dim:, :, :]
+        return obj
 
 def layer_init_with_orthogonal(layer, std=1.0, bias_const=1e-6):
     th.nn.init.orthogonal_(layer.weight, std)
@@ -103,7 +213,7 @@ def check_rnn_reg_net():
     out_dim = 8
     num_layers = 2
 
-    net = RnnRegNet(
+    net = DilatedCNNRegNet(
         inp_dim=inp_dim, mid_dim=mid_dim, out_dim=out_dim, num_layers=num_layers
     )
 
--- seq_run.py
+++ seq_run.py
@@ -28,11 +28,10 @@ class SeqData:
         # label_seq = np.nan_to_num(label_seq, nan=0.0, neginf=0.0, posinf=0.0)
         label_seq = th.tensor(label_seq, dtype=th.float32)
         assert th.isnan(label_seq).sum() == 0
-
         seq_len = label_seq.shape[0]
         self.input_dim = input_seq.shape[1]
         self.label_dim = label_seq.shape[1]
-
+        
         seq_i0 = 0
         seq_i1 = int(seq_len * train_ratio)
         seq_i2 = int(seq_len - 1024) if 0 < train_ratio < 1 else seq_len
@@ -46,13 +45,12 @@ class SeqData:
         print('SEQ Data Loaded', self.train_seq_len, self.valid_seq_len)
 
     def sample_for_train(self, batch_size: int = 32, seq_len: int = 4096, device: th.device = th.device('cpu')):
+        print('self.train_seq_len ', self.train_seq_len, 'seq len ', seq_len, 'batch size ', batch_size)
         i0s = np.random.randint(1024, self.train_seq_len - seq_len, size=batch_size)
-        print('i0s', i0s)
         ids = np.arange(seq_len)[None, :].repeat(batch_size, axis=0) + i0s[:, None]
-        print('ids', ids)
         input_ary = self.train_input_seq[ids, :].permute(1, 0, 2).to(device)
         label_seq = self.train_label_seq[ids, :].permute(1, 0, 2).to(device)
-        return input_ary, label_seq
+        return input_ary, label_seq 
 
 
 def _update_network(optimizer, obj, clip_grad_norm):
@@ -72,7 +70,7 @@ def train_model(gpu_id: int):
     num_layers = 4  # The number of layers in the recurrent network. The larger the value, the more content the recurrent network can remember.
 
     # Training duration, fitting degree
-    epoch = 2 ** 4
+    epoch = 2 ** 8
     wup_dim = 64 # The length of the sequence used for pre-warming of the recurrent network. The output loss will not be calculated during the pre-warming phase. The pre-warming phase is only used to obtain the hidden state of the recurrent network.
     valid_gap = 128  # The interval of validation data, that is, how many training batches are used to perform validation and print them out. It is recommended to print 10 to 1000 times during the entire training process
     num_patience = 8  # During model training, the number of steps that the loss can tolerate not getting a better value continuously.
@@ -90,8 +88,12 @@ def train_model(gpu_id: int):
     label_dim = seq_data.label_dim
 
     '''Model'''
-    from seq_net import RnnRegNet
+    from seq_net import RnnRegNet, DilatedCNNRegNet, TransformerRegNet
+    # Choose any one of Net model 
     net = RnnRegNet(inp_dim=input_dim, mid_dim=mid_dim, out_dim=label_dim, num_layers=num_layers).to(device)
+    net = DilatedCNNRegNet(inp_dim=input_dim, mid_dim=mid_dim, out_dim=label_dim, num_layers=num_layers).to(device)
+    net = TransformerRegNet(inp_dim=input_dim, mid_dim=mid_dim, out_dim=label_dim, num_layers=num_layers).to(device)
+
     optimizer = th.optim.AdamW(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
     criterion = th.nn.MSELoss(reduction='none')
 
@@ -183,8 +185,10 @@ def valid_model(gpu_id: int):
     predict_ary_path = args.predict_ary_path
 
     '''Model'''
-    from seq_net import RnnRegNet
+    from seq_net import RnnRegNet, DilatedCNNRegNet, TransformerRegNet
     net = RnnRegNet(inp_dim=input_dim, mid_dim=mid_dim, out_dim=label_dim, num_layers=num_layers).to(device)
+    net = DilatedCNNRegNet(inp_dim=input_dim, mid_dim=mid_dim, out_dim=label_dim, num_layers=num_layers).to(device)
+    net = TransformerRegNet(inp_dim=input_dim, mid_dim=mid_dim, out_dim=label_dim, num_layers=num_layers).to(device)
     net.load_state_dict(th.load(predict_net_path, map_location=lambda storage, loc: storage))
 
     predict_ary = np.empty_like(seq_data.valid_label_seq)
@@ -210,5 +214,5 @@ Out[3]: (1030728, 3)
 if __name__ == '__main__':
     GPU_ID = int(sys.argv[1]) if len(sys.argv) > 1 else -1  # Get GPU_ID from command line parameters
     # convert_btc_csv_to_btc_npy()  # Data preprocessing, using market information and code to generate weak factor Alpha101
-    train_model(gpu_id=GPU_ID)  # Using weak factor Alpha101 to train recurrent network RNN ​​(LSTM+GRU + Regression)
+    train_model(gpu_id=GPU_ID)  # Using advance factor to train recurrent network RNN ​​(LSTM+GRU + Regression)
     valid_model(gpu_id=GPU_ID)  # Generate prediction results using the trained recurrent network and save them to the directory specified by ConfigData
